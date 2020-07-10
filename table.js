95028c1edaf5c113c89ff8bd91ea2761b0b5381b
Michel Poirier <mpoirier101@gmail.com>
2020-07-10 16:31:14 -0400

    added table rowClick event

--- table.js
+++ table.js
@@ -6,7 +6,6 @@
   // connectedCallback() {}
   // disconnectedCallback() {}
 
-  _caption = ""; 
   _width = 0;
   _height = 0;
   _themeUrl = "";
@@ -18,6 +17,46 @@
   _sortOrder = 1;
   _sortProp = "";
   
+  init(options = {}) {
+    const me = this;
+
+    me._width = options.width || me.getAttribute("width") || "100%";
+    me._height = options.height || me.getAttribute("height") || "";
+    me._themeUrl = options.themeUrl;
+    me._theme = options.theme || "";
+    me._rowClick = options.rowClick || null;
+    me._columns = options.columns || [];
+    me._data = options.data || [];
+    me._json = options.json || "";
+
+    me.shadow = me.attachShadow({ mode: "open" });
+    me.shadow.innerHTML = `<link rel="stylesheet" type="text/css" href="/css/mp-components.css">`;
+    if (me._themeUrl) { me.shadow.innerHTML += `<link rel="stylesheet" type="text/css" href="${me._themeUrl}">`; }
+    me.shadow.innerHTML += `<div class="tableWrap"><table><thead></thead><tbody></tbody></table></div>`;
+
+    me.RowClickedEvent = new CustomEvent("rowClick", {
+      bubbles: true,
+      cancelable: false,
+      composed: true,
+      detail: { value: "" },
+    });
+
+    me.table = me.shadow.querySelector(".tableWrap");
+    if (me._width) me.table.setAttribute('width', me._width);
+    if (me._width) me.table.style.maxWidth = me._width;
+    if (me._height) me.table.setAttribute('height', me._height);
+    if (me._height) me.table.style.maxHeight = me._height;
+
+    if (me._json) {
+      me._data = JSON.parse(me._json);
+    }
+    if (me._data) {
+      this._printTable(me._data, me._columns);
+    } else {
+      table.html("no result");
+    }
+  }
+
   fillTable(items, columns) {
     const me = this;
     if (items) {
@@ -34,58 +73,17 @@
         me._columns = JSON.parse(columns);
       }
     }
-    this._printTable(me._data, me._columns);
+    this._printTable();
   }
 
-  init(options = {}) {
-    const me = this;
-
-    me._caption = options.caption || me.getAttribute("caption") || "";
-    me._width = options.width || me.getAttribute("width") || "100%";
-    me._height = options.height || me.getAttribute("height") || "";
-    me._themeUrl = options.themeUrl;
-    me._theme = options.theme || "";
-    me._rowClick = options.rowClick || null;
-    me._columns = options.columns || [];
-    me._data = options.data || [];
-    me._json = options.json || "";
-
-    me.shadow = me.attachShadow({ mode: "open" });
-    me.shadow.innerHTML = `<link rel="stylesheet" type="text/css" href="/css/mp-components.css">`;
-    if (me._themeUrl) { me.shadow.innerHTML += `<link rel="stylesheet" type="text/css" href="${me._themeUrl}">`; }
-    me.shadow.innerHTML += `<div class="tableWrap"><table><thead></thead><tbody></tbody></table></div>`;
-    
-    me.table = me.shadow.querySelector(".tableWrap");
-    if (me._width) me.table.setAttribute('width', me._width);
-    if (me._width) me.table.style.maxWidth = me._width;
-    if (me._height) me.table.setAttribute('height', me._height);
-    if (me._height) me.table.style.maxHeight = me._height;
-
-    if (me._rowClick) {
-      me.rows = me.shadow.querySelector("tbody tr");
-      me.rows.addEventListener("click", function (e) {
-        me._rowClick(this.id);
-      }, false);
-    }
-
-    if (me._json) {
-      me._data = JSON.parse(me._json);
-    }
-    if (me._data) {
-      this._printTable(me._data, me._columns);
-    } else {
-      table.html("no result");
-    }
-  }
-
-  _printTable(items, columns) {
+  _printTable() {
     const me = this;
     var thead = me.shadow.querySelector("thead");
     var tbody = me.shadow.querySelector("tbody");
     thead.innerHTML = "";
     tbody.innerHTML = "";
-    printHeader(thead, columns);
-    printRows(tbody, items, columns);
+    printHeader(thead, me._columns);
+    printRows(tbody, me._data, me._columns);
 
     function printHeader(thead, columns) {
       var tr = document.createElement('tr');
@@ -104,7 +102,11 @@
     function printRows(tbody, items, columns) {
       items.forEach(function (item, i) {
         var tr = document.createElement('tr');
-        if (item.id) tr.setAttribute('id', item.id);
+        tr.addEventListener("click", function (e) {
+          me.RowClickedEvent.detail.value = this.id;
+          me.dispatchEvent(me.RowClickedEvent);
+        }, false);
+          if (item.id) tr.setAttribute('id', item.id);
         columns.forEach(function (col, i) {
           var td = document.createElement('td');
           td.innerText = item[col.data] || "";
@@ -112,6 +114,12 @@
         });
         tbody.append(tr);
       });
+
+      //me.rows = me.shadow.querySelectorAll("tbody tr");
+      // me.rows.addEventListener("click", function (e) {
+      //   me.RowClickedEvent.detail.value = this.id;
+      //   me.dispatchEvent(me.RowClickedEvent);
+      // }, false);
     }
   }
 
@@ -124,7 +132,7 @@
       me._sortOrder = (me._sortOrder == 1) ? -1 : 1;
     }
     me._data.sort(dynamicSort(prop));
-    me._printTable(me._data, me._columns);
+    me._printTable();
 
     function dynamicSort(property) {
       return function (a, b) {
