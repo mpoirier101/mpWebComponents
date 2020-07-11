e300c80faf7f0cc29bd652b4ca24ab6d587bca20
Michel Poirier <mpoirier101@gmail.com>
2020-07-11 13:37:07 -0400

    table row click event only when ID is available

--- table.js
+++ table.js
@@ -6,32 +6,32 @@
   // connectedCallback() {}
   // disconnectedCallback() {}
 
-  _width = 0;
-  _height = 0;
-  _themeUrl = "";
   _theme = "";
-  _rowClick = null;
   _columns = [];
   _data = [];
-  _json = "";
   _sortOrder = 1;
   _sortProp = "";
   
   init(options = {}) {
     const me = this;
 
-    me._width = options.width || me.getAttribute("width") || "100%";
-    me._height = options.height || me.getAttribute("height") || "";
-    me._themeUrl = options.themeUrl;
+    var width = 0;
+    var height = 0;
+    var themeUrl = "";
+    var json = "";
+  
+    width = options.width || me.getAttribute("width") || "100%";
+    height = options.height || me.getAttribute("height") || "";
+    themeUrl = options.themeUrl;
+    
     me._theme = options.theme || "";
-    me._rowClick = options.rowClick || null;
     me._columns = options.columns || [];
     me._data = options.data || [];
-    me._json = options.json || "";
+    json = options.json || "";
 
     me.shadow = me.attachShadow({ mode: "open" });
     me.shadow.innerHTML = `<link rel="stylesheet" type="text/css" href="/css/mp-components.css">`;
-    if (me._themeUrl) { me.shadow.innerHTML += `<link rel="stylesheet" type="text/css" href="${me._themeUrl}">`; }
+    if (themeUrl) { me.shadow.innerHTML += `<link rel="stylesheet" type="text/css" href="${themeUrl}">`; }
     me.shadow.innerHTML += `<div class="tableWrap"><table><thead></thead><tbody></tbody></table></div>`;
 
     me.RowClickEvent = new CustomEvent("rowClick", {
@@ -42,18 +42,16 @@
     });
 
     me.table = me.shadow.querySelector(".tableWrap");
-    if (me._width) me.table.setAttribute('width', me._width);
-    if (me._width) me.table.style.maxWidth = me._width;
-    if (me._height) me.table.setAttribute('height', me._height);
-    if (me._height) me.table.style.maxHeight = me._height;
+    if (width) me.table.setAttribute('width', width);
+    if (width) me.table.style.maxWidth = width;
+    if (height) me.table.setAttribute('height', height);
+    if (height) me.table.style.maxHeight = height;
 
-    if (me._json) {
-      me._data = JSON.parse(me._json);
+    if (json) {
+      me._data = JSON.parse(json);
     }
     if (me._data) {
       this._printTable(me._data, me._columns);
-    } else {
-      table.html("no result");
     }
   }
 
@@ -92,9 +90,8 @@
         if (me._theme) th.classList.add(me._theme);
         th.innerHTML = col.display;
         if (col.sort) th.innerHTML += (col.sort == 1) ? " &#9650;" : " &#9660;";
-
         th.addEventListener("click", function (e) {
-          me._headClick(col.data);
+          headClick(col.data);
         }, false);
         tr.append(th);
       });
@@ -104,11 +101,13 @@
     function printRows(tbody, items, columns) {
       items.forEach(function (item, i) {
         var tr = document.createElement('tr');
-        tr.addEventListener("click", function (e) {
-          me.RowClickEvent.detail.value = this.id;
-          me.dispatchEvent(me.RowClickEvent);
-        }, false);
-          if (item.id) tr.setAttribute('id', item.id);
+        if (item.id) {
+          tr.setAttribute('id', item.id);
+          tr.addEventListener("click", function (e) {
+            me.RowClickEvent.detail.value = this.id;
+            me.dispatchEvent(me.RowClickEvent);
+          }, false);
+        }  
         columns.forEach(function (col, i) {
           var td = document.createElement('td');
           td.innerText = item[col.data] || "";
@@ -117,27 +116,26 @@
         tbody.append(tr);
       });
     }
-  }
 
-  _headClick(prop) {
-    const me = this;
-    if (prop != me._sortProp) {
-      me._sortProp = prop;
-      me._sortOrder = 1;
-    } else {
-      me._sortOrder = (me._sortOrder == 1) ? -1 : 1;
-    }
-    me._data.sort(dynamicSort(prop));
-    me._columns.map((x) => x.sort = "");
-    me._columns.find((x) => x.data == prop).sort = me._sortOrder;
-    me._printTable();
-
-    function dynamicSort(property) {
-      return function (a, b) {
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result * me._sortOrder;
+    function headClick(prop) {
+      if (prop != me._sortProp) {
+        me._sortProp = prop;
+        me._sortOrder = 1;
+      } else {
+        me._sortOrder = (me._sortOrder == 1) ? -1 : 1;
+      }
+      me._data.sort(dynamicSort(prop));
+      me._columns.map((x) => x.sort = "");
+      me._columns.find((x) => x.data == prop).sort = me._sortOrder;
+      me._printTable();
+  
+      function dynamicSort(property) {
+        return function (a, b) {
+          var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+          return result * me._sortOrder;
+        }
       }
     }
-  }
+   }
 }
 window.customElements.define("mp-table", mpTable);
